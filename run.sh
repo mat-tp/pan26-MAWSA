ae13afdc05be51aecf5ddddb0c6310a34b59d906
matlalat53@gmail.com <matlalat53@gmail.com>
2026-06-17 03:34:05 +0200

    Update the run file

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ set -e
 
 # TIRA sets these environment variables automatically
 # Use them to pass input/output directories to the Python script
-exec python3 /app/src/tira_predict.py -i "$inputDataset" -o "$outputDir"
+exec python3 /app/src/tira_predict.py "$@"
